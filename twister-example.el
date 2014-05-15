5a4a28efbf7a59fe151282e0fe925a2867fc1224
Marcel van der Boom <marcel@hsdev.com>
2014-05-15 21:40:45 +0200

    Add a little example

---
+++ twister-example.el
@@ -0,0 +1,11 @@
+;; Quick example on how to use twister.el
+
+(add-to-list 'load-path "elisp-json-rpc")
+(require 'twister)
+
+;; Either use customize or elisp directly to customize
+
+;; Fill in your own nick here
+(setq twister-nick "yournick")
+
+(twister-post "I have installed twister.el, and it works! -- https://github.com/mrvdb/twister.el")
